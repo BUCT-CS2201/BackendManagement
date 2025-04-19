77fbc0c6ecc993b94484a3f151c89480c2b679a9
Chill <791094647@qq.com>
2025-04-19 10:37:35 +0800

    后端工程环境搭建

---
+++ backend/background-management-system/src/main/java/com/example/backgroundmanagementsystem/BackgroundManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.backgroundmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackgroundManagementSystemApplication.class, args);
+	}
+
+}
