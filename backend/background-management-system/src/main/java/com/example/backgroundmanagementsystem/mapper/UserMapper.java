a0113c3a75617ca278ba91ae3e54f83a862e00d4
Chill <791094647@qq.com>
2025-04-28 12:12:29 +0800

    对数据库修改进行AOP填充

--- backend/background-management-system/src/main/java/com/example/backgroundmanagementsystem/mapper/UserMapper.java
+++ backend/background-management-system/src/main/java/com/example/backgroundmanagementsystem/mapper/UserMapper.java
@@ -1,11 +1,30 @@
 package com.example.backgroundmanagementsystem.mapper;
 
+import com.example.backgroundmanagementsystem.annotations.AutoFill;
+import com.example.backgroundmanagementsystem.enums.OperationTypeEnum;
 import com.example.backgroundmanagementsystem.pojo.entity.User;
+import com.github.pagehelper.Page;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface UserMapper {
+
+    @AutoFill(OperationTypeEnum.INSERT)
+    void insert(User user);
+
     @Select("select * from user where phone_number=#{phoneNumber}")
     User findByPhoneNumber(String phoneNumber);
+
+    @AutoFill(OperationTypeEnum.UPDATE)
+    @Update("update user set password=#{password} where user_id=#{userId}")
+    void update(User userFoUpdate);
+
+    /**
+     * 分页查询
+     * @param user
+     * @return
+     */
+    Page<User> findBatch(User user);
 }
